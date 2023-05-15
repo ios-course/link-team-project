1f48d9fd26c82f9e013f2c6e982d4004ab2387a4
Yulya Nastiushkina <y.nastiushkina@gmail.com>
2023-05-15 17:33:40 +0300

    removed unnecessary file, initial commit by CSVHandler

--- JourneysSquad/JourneysSquad/CSVError.swift
+++
@@ -1,5 +0,0 @@
-/// Enum of possible errors that may occur when working with CSV files.
-enum CSVError: Error {
-    /// File cannot be created at the specified URL.
-    case cannotCreateFile
-}
--- JourneysSquad/JourneysSquad/CSVHandler.swift
+++ JourneysSquad/JourneysSquad/CSVHandler.swift
@@ -1,34 +1,22 @@
 import Foundation
 
-/// Provides functionality to handle CSV files.
+typealias DataRecord = [String]
+
 struct CSVHandler {
+
+    init?(fileName: String) {}
     
+    func appendToTheFile(_ csvRecord: DataRecord) -> Bool {
+       return true
+    }
+    
+    func readAllData() -> [DataRecord]? {
+        return [[]]
+    }
+    
+    // MARK: - Private interface
     /// The URL path to the CSV file.
-    private let path: URL
-    
-    /// Initializes a CSVHandler with a specified file name.
-    /// - Parameter fileName: The name of the CSV file to be handled.
-    init(fileName: String) throws {
-        
-        let fileManager = FileManager.default
-        let documentsDirectory = try fileManager.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
-        let fileURL = documentsDirectory.appending(path: fileName).appendingPathExtension("csv")
-        
-        if !fileManager.fileExists(atPath: fileURL.path) {
-            let createdSuccessfully = fileManager.createFile(atPath: fileURL.path, contents: nil)
-            
-            if !createdSuccessfully {
-                throw CSVError.cannotCreateFile
-            }
-        }
-        path = fileURL
-    }
-    
-    func writeToTheFile(_: [[String]]) throws {
-        // implement
-    }
-    
-    func readAllData() throws -> [[String]] {
-        [[]]
-    }
+    private let filePath: URL
+    private let defaultEncoding = String.Encoding.utf8
+    private let defaultExtention = "csv"
 }
