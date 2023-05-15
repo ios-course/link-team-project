fdea81ff8b580cffddeed2e923979322c9466e0c
Yulya Nastiushkina <y.nastiushkina@gmail.com>
2023-05-15 17:34:29 +0300

    added logic to `init`

--- JourneysSquad/JourneysSquad/CSVHandler.swift
+++ JourneysSquad/JourneysSquad/CSVHandler.swift
@@ -4,7 +4,20 @@ typealias DataRecord = [String]
 
 struct CSVHandler {
 
-    init?(fileName: String) {}
+    init?(fileName: String) {
+        let fileManager = FileManager.default
+        guard let documentsDirectory = try? fileManager.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true) else { return nil }
+        let fileURL = documentsDirectory.appending(path: fileName).appendingPathExtension(defaultExtention)
+        
+        if !fileManager.fileExists(atPath: fileURL.path) {
+            let createdSuccessfully = fileManager.createFile(atPath: fileURL.path, contents: nil)
+            
+            if !createdSuccessfully {
+                return nil
+            }
+        }
+        filePath = fileURL
+    }
     
     func appendToTheFile(_ csvRecord: DataRecord) -> Bool {
        return true
