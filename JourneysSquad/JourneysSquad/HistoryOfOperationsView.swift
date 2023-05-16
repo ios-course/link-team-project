c15db41396fd23717f2e3950d062d12e24f1ef5c
Alex Levitski <levitski.work@gmail.com>
2023-05-16 16:00:29 +0800

    add HistoryOfOperationsView. Include title History op operations. Inculde button Add

---
+++ JourneysSquad/JourneysSquad/HistoryOfOperationsView.swift
@@ -0,0 +1,31 @@
+import SwiftUI
+
+/// A view that displays the history of operations.
+struct HistoryOfOperationsView: View {
+    var body: some View {
+        VStack {
+            Button(action: {}) { // TODO: Add action.
+                Text("Add")
+                    .font(.system(size: 20))
+                    .foregroundColor(.blue)
+            }
+            .offset(x: 160, y: 20)
+
+            Text("History of operations")
+                .font(.system(size: 20))
+                .padding(.top, 5)
+
+            Divider()
+                .background(Color.black)
+
+            Spacer()
+        }
+        .padding(.horizontal, 15)
+    }
+}
+
+struct HistoryOfOperationsView_Previews: PreviewProvider {
+    static var previews: some View {
+        HistoryOfOperationsView()
+    }
+}
