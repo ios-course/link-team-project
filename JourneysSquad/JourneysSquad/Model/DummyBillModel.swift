29e7357300298327b3b0ce9814f3b8873805d1fd
Alex Levitski <levitski.work@gmail.com>
2023-05-19 22:18:50 +0800

    separated array with participants

--- JourneysSquad/JourneysSquad/Model/DummyBillModel.swift
+++ JourneysSquad/JourneysSquad/Model/DummyBillModel.swift
@@ -7,7 +7,7 @@ struct DummyBillModel {
     let description = "Description"
 
     /// Participants who is on the account.
-    let participants = ["Participant 1, Participant 2"]
+    let participants = ["Participant 1", "Participant 2"]
 
     /// The overall sum of money that was spent within the bill.
     let sum = "50.00 $"
