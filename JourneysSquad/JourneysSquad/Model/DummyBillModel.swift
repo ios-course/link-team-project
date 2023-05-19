e437964d5c9c29c75cc573fa3042693d4e159c91
Alex Levitski <levitski.work@gmail.com>
2023-05-19 21:54:45 +0800

    added rewrited modificators in BillRowView. Added DummyBillModel

---
+++ JourneysSquad/JourneysSquad/Model/DummyBillModel.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+/// A statement of the money owed for goods or services by different people.
+struct DummyBillModel {
+
+    /// The description of the bill.
+    let description = "Description"
+
+    /// Participants who is on the account.
+    let participants = ["Participant 1, Participant 2"]
+
+    /// The overall sum of money that was spent within the bill.
+    let sum = "50.00 $"
+}
--- JourneysSquad/JourneysSquad/View/BillRowView.swift
+++ JourneysSquad/JourneysSquad/View/BillRowView.swift
@@ -1,40 +1,36 @@
 import SwiftUI
+
 /// A view displaying one bill.
 struct BillRowView: View {
+
+    var bill: DummyBillModel
 
     var body: some View {
         HStack {
             VStack(alignment: .leading) {
-                let description = "Description"
-                if description.count > 20 {
-                    Text(description.prefix(20) + "...")
-                        .font(.system(size: 14))
-                } else {
-                    Text(description)
-                        .font(.system(size: 14))
-                }
+                Text(bill.description)
+                    .lineLimit(1)
+                    .font(.system(size: 14))
 
-                let participants = ["Participant 1", "Participant 2", "Participant 3"]
-                    if participants.count > 2 {
-                        Text("\(participants[0]), \(participants[1]), ...")
-                            .foregroundColor(.gray)
-                            .font(.system(size: 14))
-                        } else {
-                            Text(participants.joined(separator: ", "))
-                                .foregroundColor(.gray)
-                                .font(.system(size: 14))
-                        }
+                Text(bill.participants.joined(separator: ", "))
+                    .lineLimit(1)
+                    .foregroundColor(.gray)
+                    .font(.system(size: 14))
             }
-            Spacer()
-            Text("20.20 BYN")
+            .padding(Edge.Set.trailing, 140)
+
+            Text(bill.sum)
                 .font(.system(size: 14))
+                .lineLimit(1)
+                .frame(maxWidth: 80)
+
         }
-        .padding(.horizontal, 10)
+        .padding()
     }
 }
 
 struct BillRowView_Previews: PreviewProvider {
     static var previews: some View {
-        BillRowView()
+        BillRowView(bill: DummyBillModel())
     }
 }
