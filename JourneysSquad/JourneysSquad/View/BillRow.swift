71616a198454360a560ed40517b1115f397cf49e
Alex Levitski <levitski.work@gmail.com>
2023-05-22 11:57:48 +0800

    added missing documentation. Add documentation for BillRowViewModel. separator for members array moved to viewmodel

--- JourneysSquad/JourneysSquad/View/BillRow.swift
+++ JourneysSquad/JourneysSquad/View/BillRow.swift
@@ -1,8 +1,8 @@
 import SwiftUI
 
-/// A view displaying one bill.
+/// A view displaying information of the one bill.
 struct BillRowView: View {
-
+    /// The view model responsible for providing data to the view.
     @ObservedObject var viewModel: BillRowViewModel
 
     var body: some View {
@@ -12,17 +12,17 @@ struct BillRowView: View {
                     .lineLimit(1)
                     .font(.system(size: 14))
 
-                Text(viewModel.dummyParticipants.joined(separator: ", "))
+                Text(viewModel.formattedParticipants)
                     .lineLimit(1)
                     .foregroundColor(.gray)
                     .font(.system(size: 14))
             }
-            .padding(Edge.Set.trailing, 100)
+            .padding(Edge.Set.trailing, 120)
 
             Text(viewModel.dummySumOfBill)
                 .font(.system(size: 14))
                 .lineLimit(1)
-                .frame(maxWidth: 80)
+                .frame(maxWidth: 50)
         }
         .padding()
     }
@@ -31,6 +31,6 @@ struct BillRowView: View {
 struct BillRow_Previews: PreviewProvider {
     static var previews: some View {
         let viewModel = BillRowViewModel()
-        return BillRowView(viewModel: viewModel)
+        BillRowView(viewModel: viewModel)
     }
 }
--- JourneysSquad/JourneysSquad/ViewModel/DummyBillRowViewModel.swift
+++ JourneysSquad/JourneysSquad/ViewModel/DummyBillRowViewModel.swift
@@ -1,8 +1,20 @@
 import Foundation
 import SwiftUI
 
+/// The view model for a bill responsible for preparing and providing data for the view.
 class BillRowViewModel: ObservableObject {
+
+    /// Description of the bill.
     @Published var dummyDescription = "Description"
+
+    /// Participants,who participate in the bill.
     @Published var dummyParticipants = ["Participant 1", "Participant 2"]
+
+    /// Total amoun of the bill.
     @Published var dummySumOfBill = "50.00"
+
+    /// A formatted string representing the members, separated by a comma.
+    var formattedParticipants: String {
+            dummyParticipants.joined(separator: ", ")
+        }
 }
