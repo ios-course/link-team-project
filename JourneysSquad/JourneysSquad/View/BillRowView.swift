461d77c8db36c15e189b4a336a1e96dfa7d39d6f
Alex Levitski <levitski.work@gmail.com>
2023-05-23 18:21:20 +0800

    added font styles constants

--- JourneysSquad/JourneysSquad/View/BillRowView.swift
+++ JourneysSquad/JourneysSquad/View/BillRowView.swift
@@ -10,21 +10,27 @@ struct BillRowView: View {
             VStack(alignment: .leading) {
                 Text(viewModel.dummyDescription)
                     .lineLimit(1)
-                    .font(.system(size: 14))
+                    .font(.system(size: bodySize))
 
                 Text(viewModel.formattedParticipants)
                     .lineLimit(1)
                     .foregroundColor(.gray)
-                    .font(.system(size: 14))
+                    .font(.system(size: noteSize))
             }
             Spacer()
 
             Text(viewModel.dummySumOfBill)
-                .font(.system(size: 14))
+                .font(.system(size: bodySize))
                 .lineLimit(1)
         }
         .padding()
     }
+    /// Font "Note" used as required [App style guidlines]
+    /// (https://github.com/ios-course/link-team-project/wiki/App-style-guidelines)
+    private let noteSize: CGFloat = 14
+    /// Font "Body" used as required [App style guidlines]
+    /// (https://github.com/ios-course/link-team-project/wiki/App-style-guidelines)
+    private let bodySize: CGFloat = 20
 }
 
 struct BillRow_Previews: PreviewProvider {
