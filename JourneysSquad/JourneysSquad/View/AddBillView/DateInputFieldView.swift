4ae9cc44e988f2b21c2b45fae044b0e343a258a6
Ivan <110284165+kirpichonok@users.noreply.github.com>
2023-05-23 18:43:09 +0300

    Add DateInputFieldView file

--- JourneysSquad/JourneysSquad/View/AddBillView/DateInputFieldView.swift
+++ JourneysSquad/JourneysSquad/View/AddBillView/DateInputFieldView.swift
@@ -1,28 +1,30 @@
 import SwiftUI
 
-struct DateFieldView: View {
+struct DateInputFieldView: View {
     let name: String
-
     @Binding var value: Date
 
     var body: some View {
         VStack(alignment: .leading) {
             Text(name.uppercased())
-                .font(.system(.subheadline, design: .rounded))
-                .fontWeight(.bold)
+                .font(.system(size: AddBillView.bodySize,
+                              weight: .bold,
+                              design: .rounded))
                 .foregroundColor(.primary)
+
+            Divider()
 
             DatePicker("", selection: $value, displayedComponents: .date)
                 .accentColor(.primary)
-                .padding(10)
                 .border(Color("Border"), width: 1.0)
                 .labelsHidden()
+                .padding(.top)
         }
     }
 }
 
 struct DateFieldView_Previews: PreviewProvider {
     static var previews: some View {
-        DateFieldView(name: "Date", value: .constant(.now))
+        DateInputFieldView(name: "Date", value: .constant(.now))
     }
 }
