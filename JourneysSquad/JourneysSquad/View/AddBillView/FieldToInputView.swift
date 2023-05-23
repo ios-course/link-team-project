759faac6b2f55415145e247b5a5b4cd4550314c6
Ivan <110284165+kirpichonok@users.noreply.github.com>
2023-05-23 18:32:10 +0300

    Add SaveButton file

--- JourneysSquad/JourneysSquad/View/AddBillView/FieldToInputView.swift
+++ JourneysSquad/JourneysSquad/View/AddBillView/FieldToInputView.swift
@@ -1,13 +1,42 @@
 import SwiftUI
 
 struct FieldToInputView: View {
+    @Binding var input: String
+    let labelOfField: String
+    let placeholder: String
+    let keyboardType: UIKeyboardType
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            Text(labelOfField.uppercased())
+                .font(.system(size: AddBillView.bodySize, weight: .bold, design: .rounded))
+
+            Divider()
+
+            TextField(placeholder, text: $input)
+                .font(.system(size: AddBillView.bodySize, weight: .medium, design: .rounded))
+                .keyboardType(.decimalPad)
+                .lineLimit(1)
+                .padding(.top)
+        }
+        .padding()
     }
+
+//    init(input: Binding<String>,
+//         labelOfField: String,
+//         placeholder: String,
+//         keyboardType: UIKeyboardType = .default) {
+//        self.input = input
+//        self.labelOfField = labelOfField
+//        self.placeholder = placeholder
+//    }
 }
 
 struct FieldToInputView_Previews: PreviewProvider {
     static var previews: some View {
-        FieldToInputView()
+        FieldToInputView(input: .constant("500"),
+                         labelOfField: "Label",
+                         placeholder: "Placeholder",
+                         keyboardType: .numberPad)
     }
 }
