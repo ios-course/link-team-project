0df200e2404fcba14c8a3993fdb37524af4e8976
Ivan <110284165+kirpichonok@users.noreply.github.com>
2023-05-24 08:57:09 +0300

    Update DataInputFieldView

--- JourneysSquad/JourneysSquad/View/AddBillView/DataInputFieldView.swift
+++ JourneysSquad/JourneysSquad/View/AddBillView/DataInputFieldView.swift
@@ -1,33 +1,46 @@
 import SwiftUI
 
+/// A field to enter text with a label above one.
 struct DataInputFieldView: View {
+    /// The text entered in the field.
     @Binding var input: String
+
+    /// The label displayed above the field.
     let labelOfField: String
+
+    /// The placeholder text displayed within the field.
     let placeholder: String
+
+    /// The type of keyboard to display when the field is tapped.
     let keyboardType: UIKeyboardType
 
     var body: some View {
         VStack(alignment: .leading) {
             Text(labelOfField.uppercased())
-                .font(.system(size: AddBillView.bodySize, weight: .bold, design: .rounded))
+                .font(.system(size: fontBodySize, weight: .bold, design: .rounded))
 
             Divider()
 
             TextField(placeholder, text: $input)
-                .font(.system(size: AddBillView.bodySize, weight: .medium, design: .rounded))
-                .keyboardType(.decimalPad)
-                .lineLimit(1)
+                .font(.system(size: fontBodySize, weight: .medium, design: .rounded))
+                .keyboardType(keyboardType)
+                .lineLimit(dataInputFieldLineLimit)
                 .padding(.top)
         }
         .padding()
     }
+
+    // MARK: - Private interface
+
+    private let fontBodySize: CGFloat = 20
+    private let dataInputFieldLineLimit = 1
 }
 
-struct FieldToInputView_Previews: PreviewProvider {
+struct DataInputFieldView_Previews: PreviewProvider {
     static var previews: some View {
-        DataInputFieldView(input: .constant("500"),
-                         labelOfField: "Label",
-                         placeholder: "Placeholder",
-                         keyboardType: .default)
+        DataInputFieldView(input: .constant(""),
+                           labelOfField: "Label",
+                           placeholder: "Placeholder",
+                           keyboardType: .default)
     }
 }
