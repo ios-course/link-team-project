6c3875fd82ccbed88976861335778cb4b5c17492
Yulya Nastiushkina <y.nastiushkina@gmail.com>
2023-05-24 13:44:22 +0300

    draft implementation

--- JourneysSquad/NavigationBarForHistoryOfOperationView.swift
+++ JourneysSquad/NavigationBarForHistoryOfOperationView.swift
@@ -1,10 +1,29 @@
 import SwiftUI
 
+/// View with a navigation bar, a title and a toolbar item.
 struct NavigationBarForHistoryOfOperationView: View {
     var body: some View {
-                Text("History of operations")
+        NavigationStack {
+            VStack {
+                Text("Any other view can be placed here")
+            }
+            .navigationTitle("History of operations")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                Text("Add")
+                    .font(.system(size: fontBodySize, design: .default))
+                    .fontWeight(.bold)
+                    .foregroundColor(.blue)
+            }
+        }
     }
+
+    // MARK: Private interface
+
+    private let fontBodySize: CGFloat = 20
 }
+
+// MARK: Internal Interface
 
 struct NavigationBarForHistoryOfOperationView_Previews: PreviewProvider {
     static var previews: some View {
