6cea007ddaffdd5055b8629e7141e71e5967b366
Yulya Nastiushkina <y.nastiushkina@gmail.com>
2023-05-24 11:21:13 +0300

    created file `NavigationBarForHistoryOfOperationView`

---
+++ JourneysSquad/NavigationBarForHistoryOfOperationView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct NavigationBarForHistoryOfOperationView: View {
+    var body: some View {
+                Text("History of operations")
+    }
+}
+
+struct NavigationBarForHistoryOfOperationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationBarForHistoryOfOperationView()
+    }
+}
