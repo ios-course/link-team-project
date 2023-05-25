9f6262933aec620bcf1bc94a0604be317a6ffdd4
Yulya Nastiushkina <y.nastiushkina@gmail.com>
2023-05-25 16:01:48 +0300

    updated implementation, fix button `Add`

--- JourneysSquad/JourneysSquad/View/NavigationBarForHistoryOfOperationView.swift
+++ JourneysSquad/JourneysSquad/View/NavigationBarForHistoryOfOperationView.swift
@@ -1,23 +1,14 @@
 import SwiftUI
 
 /// View with a navigation bar, a title and a toolbar item.
-struct NavigationBarForHistoryOfOperationView<Content: View>: View {
-    /// The title displayed in the navigation bar.
-    let title: String
-
-    /// The title displayed in the toolbar item.
-    let toolBarItemTitle: String
-
-    /// The content to be displayed within the view.
-    let content: () -> Content
-
+struct HistoryOfOperationView: View {
     var body: some View {
         NavigationStack {
-            content()
-                .navigationTitle(title)
+            Text("Any other view can be placed here")
+                .navigationTitle("History of Operations")
                 .navigationBarTitleDisplayMode(.inline)
                 .toolbar {
-                    Text(toolBarItemTitle)
+                    Button("Add") {}
                         .font(.system(size: fontBodySize))
                         .fontWeight(.bold)
                         .foregroundColor(.blue)
@@ -32,10 +23,6 @@ struct NavigationBarForHistoryOfOperationView<Content: View>: View {
 
 struct NavigationBarForHistoryOfOperationView_Previews: PreviewProvider {
     static var previews: some View {
-        NavigationBarForHistoryOfOperationView(title: "History of operations", toolBarItemTitle: "Add") {
-            VStack {
-                Text("Any other view can be placed here")
-            }
-        }
+        HistoryOfOperationView()
     }
 }
