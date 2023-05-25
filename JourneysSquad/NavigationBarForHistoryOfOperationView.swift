f723a4c070392f354437708fd29db00e474d300d
Yulya Nastiushkina <y.nastiushkina@gmail.com>
2023-05-25 11:40:45 +0300

    made code reusable

--- JourneysSquad/NavigationBarForHistoryOfOperationView.swift
+++ JourneysSquad/NavigationBarForHistoryOfOperationView.swift
@@ -1,20 +1,27 @@
 import SwiftUI
 
 /// View with a navigation bar, a title and a toolbar item.
-struct NavigationBarForHistoryOfOperationView: View {
+struct NavigationBarForHistoryOfOperationView<Content: View>: View {
+    /// The title displayed in the navigation bar.
+    let title: String
+
+    /// The title displayed in the toolbar item.
+    let toolBarItemTitle: String
+
+    /// The content to be displayed within the view.
+    let content: () -> Content
+
     var body: some View {
         NavigationStack {
-            VStack {
-                Text("Any other view can be placed here")
-            }
-            .navigationTitle("History of operations")
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbar {
-                Text("Add")
-                    .font(.system(size: fontBodySize))
-                    .fontWeight(.bold)
-                    .foregroundColor(.blue)
-            }
+            content()
+                .navigationTitle(title)
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    Text(toolBarItemTitle)
+                        .font(.system(size: fontBodySize))
+                        .fontWeight(.bold)
+                        .foregroundColor(.blue)
+                }
         }
     }
 
@@ -25,6 +32,10 @@ struct NavigationBarForHistoryOfOperationView: View {
 
 struct NavigationBarForHistoryOfOperationView_Previews: PreviewProvider {
     static var previews: some View {
-        NavigationBarForHistoryOfOperationView()
+        NavigationBarForHistoryOfOperationView(title: "History of operations", toolBarItemTitle: "Add") {
+            VStack {
+                Text("Any other view can be placed here")
+            }
+        }
     }
 }
