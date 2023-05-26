ce9194d3899744a71d679c483f77e178aff9ee93
Yulya Nastiushkina <y.nastiushkina@gmail.com>
2023-05-26 18:11:01 +0300

    fixed doc comments

--- JourneysSquad/JourneysSquad/View/ReusableStyles/NavigationBarItemBlue.swift
+++ JourneysSquad/JourneysSquad/View/ReusableStyles/NavigationBarItemBlue.swift
@@ -1,8 +1,6 @@
 import SwiftUI
 
-/// A button style that applies a blue color, bold font, and a body font size
-/// that specified on [App Style Guidelines](https://github.com/ios-course/link-team-project/wiki/App-Style-Guidelines)
-/// to the button label.
+/// This button style is transparent button with blue text and bold font.
 struct NavigationBarItemBlue: ButtonStyle {
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
