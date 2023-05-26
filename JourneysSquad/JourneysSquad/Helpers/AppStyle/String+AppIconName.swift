6d64f7ecbdda6a017c5f6086745ad7e08c8f4c2b
Ivan <110284165+kirpichonok@users.noreply.github.com>
2023-05-26 18:48:23 +0300

    Rename and update BlackCircleWithPlusButton. Update

--- JourneysSquad/JourneysSquad/Helpers/AppStyle/String+AppIconName.swift
+++ JourneysSquad/JourneysSquad/Helpers/AppStyle/String+AppIconName.swift
@@ -1,5 +1,10 @@
 extension String {
+    /// The set of names for using UI elements such as buttons, systemImages.
     enum AppIconName {
+        /// The name used to create an image with a plus sign.
         static let plus = "plus"
+
+        /// The text for `Save` button label.
+        static let save = "Save"
     }
 }
