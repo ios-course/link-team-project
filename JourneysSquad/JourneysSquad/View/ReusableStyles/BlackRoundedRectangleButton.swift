8e0d76c7942c19c22bdd5fc7a8a14dda1148a4e0
Ivan <110284165+kirpichonok@users.noreply.github.com>
2023-05-28 21:07:03 +0300

    Update BlackRoundedRectangleButton: delete static

--- JourneysSquad/JourneysSquad/View/ReusableStyles/BlackRoundedRectangleButton.swift
+++ JourneysSquad/JourneysSquad/View/ReusableStyles/BlackRoundedRectangleButton.swift
@@ -4,23 +4,23 @@ import SwiftUI
 struct BlackRoundedRectangleButton: ButtonStyle {
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
-            .font(.system(size: BlackRoundedRectangleButton.fontBodySize))
+            .font(.system(size: fontBodySize))
             .fontWeight(.bold)
             .frame(
-                width: BlackRoundedRectangleButton.buttonWidth,
-                height: BlackRoundedRectangleButton.buttonHeight
+                width: buttonWidth,
+                height: buttonHeight
             )
             .background(.black)
             .foregroundColor(.white)
-            .cornerRadius(BlackRoundedRectangleButton.buttonCornerRadius)
+            .cornerRadius(buttonCornerRadius)
     }
 
     // MARK: - Private interface
 
-    private static let fontBodySize: CGFloat = 20
-    private static let buttonWidth: CGFloat = 150
-    private static let buttonHeight: CGFloat = 40
-    private static let buttonCornerRadius: CGFloat = 15
+    private let fontBodySize: CGFloat = 20
+    private let buttonWidth: CGFloat = 150
+    private let buttonHeight: CGFloat = 40
+    private let buttonCornerRadius: CGFloat = 15
 }
 
 struct BlackRoundedRectangleButton_Previews: PreviewProvider {
