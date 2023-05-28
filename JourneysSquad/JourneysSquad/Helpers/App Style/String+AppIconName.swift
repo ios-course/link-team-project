e9b7b22e49687b0ee3cd5ac199403f6df2451e4b
Ivan <110284165+kirpichonok@users.noreply.github.com>
2023-05-28 20:22:54 +0300

    Add AppIconName, BlackCircleWithPlusButton, BlackRoundedRectangleButton

---
+++ JourneysSquad/JourneysSquad/Helpers/App Style/String+AppIconName.swift
@@ -0,0 +1,10 @@
+extension String {
+    /// The set of names for using UI elements.
+    enum AppIconName {
+        /// The name used to create an image with a plus sign.
+        static let plus = "plus"
+
+        /// The text for `Save` button label.
+        static let save = "Save"
+    }
+}
