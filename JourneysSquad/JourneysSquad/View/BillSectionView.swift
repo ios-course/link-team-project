caab8134380d67675ff8a86715ad3bd3bbf81c47
Ivan <110284165+kirpichonok@users.noreply.github.com>
2023-05-29 11:45:12 +0300

    Add file BillSectionView

---
+++ JourneysSquad/JourneysSquad/View/BillSectionView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct BillSectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BillSectionView_Previews: PreviewProvider {
+    static var previews: some View {
+        BillSectionView()
+    }
+}
