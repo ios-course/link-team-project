c796653b59c9eccd42471382cd46b1fd7bc37d99
Yulya Nastiushkina <y.nastiushkina@gmail.com>
2023-05-29 18:46:04 +0300

    added documentation for `enum AppIconName`

--- JourneysSquad/JourneysSquad/Helpers/AppStyle/String+AppIconName.swift
+++ JourneysSquad/JourneysSquad/Helpers/AppStyle/String+AppIconName.swift
@@ -1,5 +1,9 @@
 extension String {
+    
+    // Predefined app icon names used in the application.
     enum AppIconName {
+        
+        // The name of the app icon representing a backward chevron.
         static let backwardChevron = "chevron.backward"
     }
 }
