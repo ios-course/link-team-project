e7f130db1a9ea363289902347d9931231332fc0a
Ivan <110284165+kirpichonok@users.noreply.github.com>
2023-05-29 09:22:35 +0300

    update AppIconName: replace plus with plus.circle. Rename BlackCircleWithPlusButton to CircleWithPlusButton, update plus image.

--- JourneysSquad/JourneysSquad/Helpers/App Style/String+AppIconName.swift
+++ JourneysSquad/JourneysSquad/Helpers/App Style/String+AppIconName.swift
@@ -1,8 +1,8 @@
 extension String {
     /// The set of names for using UI elements.
     enum AppIconName {
-        /// The name used to create an image with a plus sign.
-        static let plus = "plus"
+        /// The name used to create an image as a circle with a plus sign in the center.
+        static let plus = "plus.circle"
 
         /// The text for `Save` button label.
         static let save = "Save"
---
+++ JourneysSquad/JourneysSquad/View/ReusableStyles/CircleWithPlusButton.swift
@@ -0,0 +1,24 @@
+import SwiftUI
+
+/// Create a button as a circle with a plus in the center.
+struct CircleWithPlusButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+        Image(systemName: String.AppIconName.plus)
+            .resizable()
+            .frame(width: frameWidth,
+                   height: frameHeight)
+    }
+
+    // MARK: - Private interface
+
+    private let frameWidth: CGFloat = 35
+    private var frameHeight: CGFloat = 35
+}
+
+struct CircleWithPlusButton_Previews: PreviewProvider {
+    static var previews: some View {
+        Button("") {}
+            .buttonStyle(CircleWithPlusButton())
+    }
+}
