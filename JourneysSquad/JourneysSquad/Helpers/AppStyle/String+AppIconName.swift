25e8ef9e9e71270702d30301114049adb9ae9c5b
Ivan <110284165+kirpichonok@users.noreply.github.com>
2023-05-30 09:05:04 +0300

    Add AppIconNameTests. Add AppButtonLabelName

---
+++ JourneysSquad/JourneysSquad/Helpers/AppStyle/String+AppIconName.swift
@@ -0,0 +1,7 @@
+extension String {
+    /// The names of icons to use in the app.
+    enum AppIconName {
+        /// The name of the icon to show "A circle with a plus sign in the center".
+        static let plus = "plus.circle"
+    }
+}
---
+++ JourneysSquad/JourneysSquad/Helpers/String+AppButtonLabelName.swift
@@ -0,0 +1,8 @@
+extension String {
+    /// The text for button labels to use in the app.
+    enum AppButtonLabelName {
+
+        /// The text for `Save` button label.
+        static let save = "Save"
+    }
+}
