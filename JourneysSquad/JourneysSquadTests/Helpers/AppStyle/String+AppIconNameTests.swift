bad37e8716bbe074f9e0b3eb43380010b901397d
Yulya Nastiushkina <y.nastiushkina@gmail.com>
2023-05-30 10:04:48 +0300

    fixed nit

--- JourneysSquad/JourneysSquadTests/Helpers/AppStyle/String+AppIconNameTests.swift
+++ JourneysSquad/JourneysSquadTests/Helpers/AppStyle/String+AppIconNameTests.swift
@@ -1,10 +1,8 @@
 import XCTest
 
 final class AppIconNameTests: XCTestCase {
-    func testAppIconNameBackwardChevronEqualSystemImageChevronBackward() {
-        let iconName = String.AppIconName.backwardChevron
-        let systemImageName = "chevron.backward"
-
-        XCTAssertEqual(iconName, systemImageName)
+    func testAppIconNamesHaveExpectedValue() {
+        XCTAssertEqual(String.AppIconName.backwardChevron, "chevron.backward")
+        // add new checks later...
     }
 }
