c1d2e0f47366fde9c9ff1b2d7a2b69417d5926f6
Ivan <110284165+kirpichonok@users.noreply.github.com>
2023-05-30 09:05:04 +0300

    Add AppIconNameTests. Add AppButtonLabelName
    # Conflicts:
    #	JourneysSquad/JourneysSquad.xcodeproj/project.pbxproj

---
+++ JourneysSquad/JourneysSquadTests/Helpers/AppStyle/AppIconNameTests.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+import XCTest
+
+final class AppIconNameTests: XCTestCase {
+    func testAppIconNameReturnsCorrectValue() {
+        let plusSystemName = "plus.circle"
+
+        XCTAssertEqual(plusSystemName, String.AppIconName.plus)
+    }
+}
