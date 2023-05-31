7b6da7c1cad660c0b7277077b22cc2af00614c3b
Ivan <110284165+kirpichonok@users.noreply.github.com>
2023-05-31 14:38:46 +0300

    Update StringAppIconNameTests after review.

--- JourneysSquad/JourneysSquadTests/Helpers/AppStyle/String+AppIconNameTests.swift
+++ JourneysSquad/JourneysSquadTests/Helpers/AppStyle/String+AppIconNameTests.swift
@@ -3,7 +3,5 @@ import XCTest
 final class StringAppIconNameTests: XCTestCase {
     func testAppIconNamesHaveExpectedValues() {
         XCTAssertEqual(String.AppIconName.circleWithPlusInCenter, "plus.circle")
-
-        // add new checks later...
     }
 }
