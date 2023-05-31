484b9501bb532567c8cdf02ee0bec724e07cb60c
Ivan <110284165+kirpichonok@users.noreply.github.com>
2023-05-31 14:55:55 +0300

    Update CircleWithPlusButton after review.

--- JourneysSquad/JourneysSquad/View/ReusableStyles/CircleWithPlusButton.swift
+++ JourneysSquad/JourneysSquad/View/ReusableStyles/CircleWithPlusButton.swift
@@ -5,33 +5,17 @@ struct CircleWithPlusButton: ButtonStyle {
     func makeBody(configuration _: Configuration) -> some View {
         Image(systemName: .AppIconName.circleWithPlusInCenter)
             .resizable()
-            .frame(width: frameWidth,
-                   height: frameHeight)
     }
-
-    /// Creates a circle button. The circle is inscribed in a rectangle
-    /// frame with the specified width and height.
-    ///
-    /// By default the frame width and height equal to 35 points.
-    ///
-    /// - Parameters:
-    ///  - width: The frame width in which the button is inscribed.
-    ///  - height: The frame height in which the button is inscribed.
-    init(width: CGFloat = 35,
-         height: CGFloat = 35) {
-        frameWidth = width
-        frameHeight = height
-    }
-
-    // MARK: - Private interface
-
-    private let frameWidth: CGFloat
-    private let frameHeight: CGFloat
 }
 
 struct CircleWithPlusButton_Previews: PreviewProvider {
     static var previews: some View {
         Button("") {}
             .buttonStyle(CircleWithPlusButton())
+            .frame(width: 100, height: 100)
+        
+        Button("") {}
+            .buttonStyle(CircleWithPlusButton())
+            .frame(width: 10, height: 10)
     }
 }
