71b11e325fe7e3a4de0f3cf6d0e71ab19f7a8666
Yulya Nastiushkina <y.nastiushkina@gmail.com>
2023-05-31 19:11:56 +0300

    added logic to method `appendTheBill`

--- JourneysSquad/JourneysSquad/Model/BillStorage/BillStorage.swift
+++ JourneysSquad/JourneysSquad/Model/BillStorage/BillStorage.swift
@@ -2,15 +2,37 @@ import Foundation
 
 class BillStorage {
     static let shared = BillStorage()
-    private(set) var bills: [Bill]
+    private(set) var bills: [Bill] = []
+    var head: BillNode?
     
     private init() {
-        self.bills = []
+        self.head = nil
     }
     
     func appendTheBill(_ newBill: Bill) -> Bool {
-        let initialBillCount = bills.count
-        bills.append(newBill)
-        return bills.count > initialBillCount
+        let newNode = BillNode(bill: newBill)
+        
+        if head == nil {
+            head = newNode
+            return true
+        } else {
+            var currentNode = head
+            var previous: BillNode? = nil
+            
+            while let currentBill = currentNode, currentBill.bill.date >= newBill.date {
+                previous = currentNode
+                currentNode = currentBill.next
+            }
+            
+            if previous == nil {
+                newNode.next = head
+                head = newNode
+            } else {
+                previous?.next = newNode
+                newNode.next = currentNode
+                return true
+            }
+        }
+        return false
     }
 }
