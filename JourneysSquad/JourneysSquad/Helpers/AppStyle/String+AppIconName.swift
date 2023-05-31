325f6c02b2535199e337d0b01fb4cac14caf129f
Ivan <110284165+kirpichonok@users.noreply.github.com>
2023-05-31 09:21:19 +0300

    Update AppIconName and StringAppIconNameTests according to remote.

--- JourneysSquad/JourneysSquad/Helpers/AppStyle/String+AppIconName.swift
+++ JourneysSquad/JourneysSquad/Helpers/AppStyle/String+AppIconName.swift
@@ -1,9 +1,6 @@
 extension String {
-    // Predefined app icon names used in the application.
+    /// Predefined app icon names used in the application.
     enum AppIconName {
-        // The name of the app icon representing a backward chevron.
-        static let backwardChevron = "chevron.backward"
-
         /// The name of the icon to show "A circle with a plus sign in the center".
         static let circleWithPlusInCenter = "plus.circle"
     }
--- JourneysSquad/JourneysSquadTests/Helpers/AppStyle/String+AppIconNameTests.swift
+++ JourneysSquad/JourneysSquadTests/Helpers/AppStyle/String+AppIconNameTests.swift
@@ -2,7 +2,6 @@ import XCTest
 
 final class StringAppIconNameTests: XCTestCase {
     func testAppIconNamesHaveExpectedValues() {
-        XCTAssertEqual(String.AppIconName.backwardChevron, "chevron.backward")
         XCTAssertEqual(String.AppIconName.circleWithPlusInCenter, "plus.circle")
 
         // add new checks later...
