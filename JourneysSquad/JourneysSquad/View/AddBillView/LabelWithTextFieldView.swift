f42b1c4f5d8b90bb227b3c4defbad2be683a784b
Ivan <110284165+kirpichonok@users.noreply.github.com>
2023-05-31 15:57:08 +0300

    Update LabelWithTextFieldView after review. Move files to one View folder.

--- JourneysSquad/JourneysSquad/View/AddBillView/LabelWithTextFieldView.swift
+++ JourneysSquad/JourneysSquad/View/AddBillView/LabelWithTextFieldView.swift
@@ -40,19 +40,26 @@ struct LabelWithTextFieldView: View {
 
 struct LabelWithTextFieldView_Previews: PreviewProvider {
     static var previews: some View {
-        LabelWithTextFieldView(
-            input: .constant(""),
-            label: "Label",
-            placeholder: "Placeholder",
-            keyboardType: .default,
-            alignment: .leading
-        )
-        LabelWithTextFieldView(
-            input: .constant("Here's a field with a quite long fancy text that doesn't fit on one line."),
-            label: "Here's a label with a quite long fancy text that doesn't fit on one line.",
-            placeholder: "Placeholder",
-            keyboardType: .default,
-            alignment: .leading
-        )
+        VStack {
+            Spacer()
+            LabelWithTextFieldView(
+                input: .constant(""),
+                label: "Label",
+                placeholder: "Placeholder",
+                keyboardType: .default,
+                alignment: .leading
+            )
+
+            Spacer()
+            LabelWithTextFieldView(
+                input: .constant("Here's a field with a quite long fancy text that doesn't fit on one line."),
+                label: "Here's a label with a quite long fancy text that doesn't fit on one line.",
+                placeholder: "Placeholder",
+                keyboardType: .default,
+                alignment: .leading
+            )
+
+            Spacer()
+        }
     }
 }
