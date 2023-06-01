3c182ddc18b30e39baba854d7762aff27dc4cc61
Yulya Nastiushkina <y.nastiushkina@gmail.com>
2023-06-01 11:56:58 +0300

    remove force unwrap

--- JourneysSquad/JourneysSquad/Model/BillStorage/BillStorage.swift
+++ JourneysSquad/JourneysSquad/Model/BillStorage/BillStorage.swift
@@ -2,10 +2,9 @@ import Foundation
 
 /// Storage to store bills.
 class BillStorage {
-    
     /// The shared instance of the bill storage.
     static let shared = BillStorage()
-    
+
     /// Inserts a bill into the storage based on its date.
     /// - Parameter bill: The bill to be inserted.
     func insertBill(_ bill: Bill) {
@@ -13,21 +12,22 @@ class BillStorage {
             newBill.date >= listBill.date
         }
     }
-    
+
     /// Retrieves all the bills from the storage.
     /// - Returns: An array containing all the bills in the storage.
     func getAllBills() -> [Bill] {
         var allBills = [Bill]()
         var currentNode = bills.head
-        
-        while currentNode != nil {
-            allBills.append(currentNode!.value)
-            currentNode = currentNode?.next
+
+        while let node = currentNode {
+            allBills.append(node.value)
+            currentNode = node.next
         }
-        
+
         return allBills
     }
-   
+
     // MARK: - Private interface
+
     private(set) var bills = LinkedList<Bill>()
 }
