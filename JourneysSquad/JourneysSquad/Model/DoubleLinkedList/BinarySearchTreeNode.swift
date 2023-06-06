cd103513c4a322ac80ec40744b68c61669b28c0e
Yulya Nastiushkina <y.nastiushkina@gmail.com>
2023-06-06 13:00:31 +0300

    implementation of `BinarySearchTree` and docs

--- JourneysSquad/JourneysSquad/Model/DoubleLinkedList/BinarySearchTreeNode.swift
+++ JourneysSquad/JourneysSquad/Model/DoubleLinkedList/BinarySearchTreeNode.swift
@@ -2,13 +2,13 @@
 final class BinarySearchTreeNode<T> {
     /// The value stored in the node.
     var value: T
-    
+
     /// The left child node of the current node.
     var left: BinarySearchTreeNode<T>?
-    
+
     /// The right child node of the current node.
     var right: BinarySearchTreeNode<T>?
-    
+
     /// Creates a new node with a specified value.
     ///
     /// - Parameter value: The value to be stored in the node.
