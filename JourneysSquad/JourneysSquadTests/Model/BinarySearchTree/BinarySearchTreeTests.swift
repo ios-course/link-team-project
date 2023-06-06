c4abf78a35f17d72d23b575c5c7ef72b60e73a57
Yulya Nastiushkina <y.nastiushkina@gmail.com>
2023-06-06 13:42:11 +0300

    added tests

--- JourneysSquad/JourneysSquadTests/Model/BinarySearchTree/BinarySearchTreeTests.swift
+++ JourneysSquad/JourneysSquadTests/Model/BinarySearchTree/BinarySearchTreeTests.swift
@@ -1,138 +1,114 @@
 import XCTest
 
-final class DoubleLinkedListTests: XCTestCase {
-    func testAppendingToTheEmptyList() {
-        var linkedList = DoubleLinkedList<Int>()
+private struct Data {
+    let date: Date
+    let values: [String]
+}
 
-        linkedList.append(value: 10)
+extension Data: Comparable {
+    static func < (lhs: Data, rhs: Data) -> Bool {
+        lhs.date < rhs.date
+    }
+}
 
-        XCTAssertEqual(linkedList.head?.value, 10)
-        XCTAssertEqual(linkedList.tail?.value, 10)
+final class BinarySearchTreeTests: XCTestCase {
+    private var bst = BinarySearchTree<Data>()
+
+    override func setUp() {
+        bst = BinarySearchTree<Data>()
     }
 
-    func testAppendingMultipleValues() {
-        var linkedList = DoubleLinkedList<Int>()
-
-        linkedList.append(value: 10)
-        linkedList.append(value: 5)
-        linkedList.append(value: 0)
-
-        XCTAssertEqual(linkedList.head?.value, 10)
-        XCTAssertEqual(linkedList.tail?.value, 0)
+    override func tearDown() {
+        bst.removeAll()
     }
 
-    func testAppendingWithDifferentType() {
-        var linkedList = DoubleLinkedList<String>()
-
-        linkedList.append(value: "Apple")
-        linkedList.append(value: "Mango")
-        linkedList.append(value: "Lemon")
-
-        XCTAssertEqual(linkedList.head?.value, "Apple")
-        XCTAssertEqual(linkedList.tail?.value, "Lemon")
+    func testEmptyTree() {
+        XCTAssertTrue(bst.values().isEmpty)
     }
 
-    func testInsertionToTheEmptyList() {
-        var linkedList = DoubleLinkedList<Int>()
+    func testMethodInsertCorrectlyInsertsEachValue() {
+        let data = [
+            Data(date: .now.addingTimeInterval(0), values: ["A", "B", "C"]),
+            Data(date: .now.addingTimeInterval(20), values: ["D", "E"]),
+            Data(date: .now.addingTimeInterval(-20), values: ["F", "G", "H"]),
+        ]
 
-        linkedList.insert(value: 5, before: nil)
+        bst = BinarySearchTree()
+        data.forEach { bst.insert($0) }
 
-        XCTAssertEqual(linkedList.head?.value, 5)
-        XCTAssertEqual(linkedList.tail?.value, 5)
+        XCTAssertEqual(bst.values(), data.sorted())
     }
 
-    func testInsertionWithMultipleValuesWhenNodeBeforeInsertIsNil() {
-        var linkedList = DoubleLinkedList<Int>()
+    func testMethodInsertHandleDuplicateValue() {
+        let data = [
+            Data(date: .now.addingTimeInterval(0), values: ["A", "B", "C"]),
+            Data(date: .now.addingTimeInterval(20), values: ["D", "E"]),
+            Data(date: .now.addingTimeInterval(0), values: ["A", "B", "C"]),
+        ]
 
-        linkedList.append(value: 3)
-        linkedList.append(value: 7)
-        linkedList.append(value: 10)
-        linkedList.insert(value: 5, before: nil)
+        bst = BinarySearchTree()
+        data.forEach { bst.insert($0) }
 
-        XCTAssertEqual(linkedList.head?.value, 5)
+        XCTAssertEqual(bst.values(), data.sorted())
     }
 
-    func testInsertionBeforeHead() {
-        var linkedList = DoubleLinkedList<Int>()
+    func testMethodValuesReturnsValuesInCorrectOrder() {
+        let data = [
+            Data(date: .now.addingTimeInterval(0), values: ["A", "B", "C"]),
+            Data(date: .now.addingTimeInterval(20), values: ["D", "E"]),
+            Data(date: .now.addingTimeInterval(-20), values: ["F", "G", "H"]),
+        ]
 
-        linkedList.append(value: 10)
-        guard let head = linkedList.head else {
-            XCTFail("Head node is nil")
-            return
-        }
+        bst = BinarySearchTree(array: data)
 
-        linkedList.insert(value: 5, before: head)
-
-        XCTAssertEqual(linkedList.head?.value, 5)
-        XCTAssertEqual(linkedList.tail?.value, 10)
+        XCTAssertEqual(bst.values(), data.sorted())
     }
 
-    func testInsertionBeforeHeadWithMultipleValues() {
-        var linkedList = DoubleLinkedList<Int>()
+    func testMethodFirstReturnsNilWhenTheTreeIsEmpty() {
+        let searchResult = bst.first(where: { $0.value.date == .now.addingTimeInterval(100) })
 
-        linkedList.append(value: 10)
-        linkedList.append(value: 5)
-        linkedList.append(value: 0)
-
-        guard let head = linkedList.head else {
-            XCTFail("Head node is nil")
-            return
-        }
-
-        linkedList.insert(value: 15, before: head)
-
-        XCTAssertEqual(linkedList.head?.value, 15)
+        XCTAssertNil(searchResult)
     }
 
-    func testInsertionBeforeNode() {
-        var linkedList = DoubleLinkedList<Int>()
+    func testMethodFirstReturnsTheCorrectNode() {
+        let data = [
+            Data(date: .now.addingTimeInterval(0), values: ["A", "B", "C"]),
+            Data(date: .now.addingTimeInterval(20), values: ["D", "E"]),
+            Data(date: .now.addingTimeInterval(-20), values: ["F", "G", "H"]),
+        ]
 
-        linkedList.append(value: 10)
-        linkedList.append(value: 5)
+        bst = BinarySearchTree(array: data)
 
-        guard let tail = linkedList.tail else {
-            XCTFail("Tail node is nil")
-            return
-        }
+        let searchResult = bst.first(where: { $0.value.date == data[1].date })
 
-        linkedList.insert(value: 6, before: tail)
-
-        XCTAssertEqual(linkedList.head?.value, 10)
-        XCTAssertEqual(linkedList.tail?.value, 5)
-        XCTAssertEqual(linkedList.head?.next?.value, 6)
+        XCTAssertEqual(searchResult?.value, data[1])
     }
 
-    func testInsertionBeforeNodeWithMultipleValues() {
-        var linkedList = DoubleLinkedList<Int>()
+    func testMethodFirstReturnsNilWhenThereIsNoSatisfyingNode() {
+        let data = [
+            Data(date: .now.addingTimeInterval(0), values: ["A", "B", "C"]),
+            Data(date: .now.addingTimeInterval(20), values: ["D", "E"]),
+            Data(date: .now.addingTimeInterval(-20), values: ["F", "G", "H"]),
+        ]
 
-        linkedList.append(value: 10)
-        linkedList.append(value: 5)
-        linkedList.append(value: 0)
+        bst = BinarySearchTree(array: data)
 
-        guard let nodeToInsertBefore = linkedList.head?.next else {
-            XCTFail("The node before to insert is nil")
-            return
-        }
-        linkedList.insert(value: 6, before: nodeToInsertBefore)
+        let searchResult = bst.first(where: { $0.value.date == .now.addingTimeInterval(100) })
 
-        XCTAssertEqual(linkedList.head?.next?.value, 6)
+        XCTAssertNil(searchResult)
     }
 
-    func testInsertionWithDifferentType() {
-        var linkedList = DoubleLinkedList<String>()
+    func testRemoveAllNodes() {
+        let data = [
+            Data(date: .now.addingTimeInterval(0), values: ["A", "B", "C"]),
+            Data(date: .now.addingTimeInterval(20), values: ["D", "E"]),
+            Data(date: .now.addingTimeInterval(-20), values: ["F", "G", "H"]),
+        ]
 
-        linkedList.append(value: "B")
-        linkedList.append(value: "C")
-        linkedList.append(value: "D")
+        bst = BinarySearchTree()
+        data.forEach { bst.insert($0) }
+        bst.removeAll()
 
-        guard let head = linkedList.head else {
-            XCTFail("Head node is nil")
-            return
-        }
-
-        linkedList.insert(value: "A", before: head)
-
-        XCTAssertEqual(linkedList.head?.value, "A")
-        XCTAssertEqual(linkedList.tail?.value, "D")
+        XCTAssertTrue(bst.values().isEmpty)
     }
 }
